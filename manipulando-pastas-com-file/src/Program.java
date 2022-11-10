054306592612968a96d195155415a272fabd979e
Michelle Madeira <michellemadeira1@gmail.com>
2022-11-10 12:09:57 -0300

    Manipulando-pastas-com-file

---
+++ manipulando-pastas-com-file/src/Program.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main (String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File :: isDirectory);
+		System.out.println("Folders: ");
+		
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File :: isFile);
+		System.out.println("Files: ");
+		
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean sucess = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully");
+		
+		sc.close();
+	}
+}
